2d64e32401be702a4b00ac1269d23cfd33e58654
David Balsas Martín <dasdadbm@gmail.com>
2018-11-26 22:44:23 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function busquedaon(){ 
+	document.busquedaon.submit();
+}
+
+function tag(id)	{return document.getElementById(id);}
+function mostrar(que)	{tag(que).style.visibility = "visible";}
+function ocultar(que)	{tag(que).style.visibility = "hidden";}
+
+function link(ciudad){ 
+	//alert(ciudad);
+	document.getElementById("buscaCiudad").value = ciudad;
+	document.formulario1.submit();
+} 
